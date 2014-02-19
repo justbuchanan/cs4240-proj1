d640da8968e916da4cacf801f606dab872860fba
Justin Buchanan <justbuchanan@gmail.com>
2014-02-19 12:16:27 -0500

    moved states to their own file

---
+++ State.java
@@ -0,0 +1,77 @@
+
+//	accept states are token types
+//	TODO: reorder to group accept states together
+//	COMMA and below are accept states
+public enum State {
+	START,
+
+	_COMMENT_END,
+	STRLIT_PART,
+	COMMENT_BEGIN,
+	STRLIT_SLASH,
+	STRLIT_SLASH_CONTROL,
+	STRLIT_SLASH_DECIMAL1,
+	STRLIT_SLASH_DECIMAL2,
+
+	COMMA,
+	COLON,
+	SEMI,
+	LPAREN,
+	RPAREN,
+	LBRACK,
+	RBRACK,
+	LBRACE,
+	RBRACE,
+	PERIOD,
+	MINUS,
+	MULT,
+	DIV,
+	EQ,
+
+	LESSER,
+	NEQ,
+	GREATER,
+	LESSEREQ,
+	GREATEREQ,
+	AND,
+	OR,
+
+	ASSIGN,
+
+	COMMENT,
+
+	ID,
+	INTLIT,
+	STRLIT,
+
+
+
+	//	keywords - they're not really states, but they are token types
+	ARRAY,
+	BREAK,
+	DO,
+	ELSE,
+	END,
+	FOR,
+	FUNC,
+	IF,
+	IN,
+	LET,
+	NIL,
+	OF,
+	THEN,
+	TO,
+	TYPE,
+	VAR,
+	WHILE,
+	ENDIF,
+	BEGIN,
+	ENDDO,
+
+
+
+	ERROR,
+}
+
+
+
