e6c61970006cde7801cf31e37129952a606ac37b
Justin Buchanan <justbuchanan@gmail.com>
2014-03-03 23:57:58 -0500

    implemented buildParserTable().  closes #5

--- Parser.java
+++ Parser.java
@@ -3,7 +3,6 @@ import java.util.LinkedList;
 public class Parser{
 	
 	private Scanner scanner;
-	private LinkedList<ParserSymbol> symbolStack;
 	private ProductionRule[][] parserTable;
 	private Grammar grammar;
 
@@ -11,14 +10,14 @@ public class Parser{
 		this.scanner = scanner;
 		symbolStack = new LinkedList();
 		this.grammar = grammar;
+		buildParserTable();
 	}
 
 	public void parseText(){
-		buildParserTable();
-				//	print out all tokens
-		
+		Stack<ParserSymbol> symbolStack = new Stack<>();
 		symbolStack.push(new TerminalParserSymbol(Terminals.$));
 		symbolStack.push(new NonTerminalParserSymbol(NonTerminals.TIGER_PROGRAM));
+
 		while(!symbolStack.isEmpty()){
 			ParserSymbol next = symbolStack.pop();
 			if(next instanceof TerminalParserSymbol){
@@ -51,17 +50,27 @@ public class Parser{
 		}
 	}
 
-	// TODO: SHOULD parserTable[n][0] be COMMA (our first accept state) FROM STATES TABLE
 	private void buildParserTable(){
-		for(int nonTerminalIndex = 0; nonTerminalIndex < parserTable.length; nonTerminalIndex++){
-			for(int terminalIndex = 0; terminalIndex < parserTable[0].length; terminalIndex++){
-				parserTable[nonTerminalIndex][terminalIndex] = ProductionRuleFactory.getProdRule(NonTerminals.values()[nonTerminalIndex],
-						Terminals.values()[terminalIndex]);
+		parserTable = new ProductionRule[NUM_NONTERMINALS][NUM_TERMINALS];
+
+		TerminalParserSymbol nullSymbol = new TerminalParserSymbol(Terminals.NULL);
+
+		//	add entries based on first && follow sets
+		for (ProductionRule rule : grammar.allRules()) {
+			Set<TerminalParserSymbol> firstSet = grammar.findFirstSet(rule);
+			for (TerminalParserSymbol terminal : firstSet) {
+				parserTable[rule.left().ordinal()][terminal.ordinal()] = rule;
+			}
+
+			if (firstSet.contains(nullSymbol)) {
+				NonTerminalParserSymbol nonterminal = rule.left();
+				Set<TerminalParserSymbol> followSet = grammar.findFollowSet(nonterminal);
+
+				for (TerminalParserSymbol terminal : followSet) {
+					parserTable[nonterminal.ordinal()][terminal.ordinal()] = nullSymbol;
+				}
 			}
 		}
 	}
-
-
-	
 
 }
