justbuchanan/cs4240-proj1

commit c78bef5a620e58c63c170d516fb1f9401381933f

foundation for symbol table in place. Need to iron out bugs and finish implementing functions

Parser.java
@@ -11,16 +11,21 @@ public class Parser{
 	private int NUM_NONTERMINALS = 49;
 	private int NUM_TERMINALS = 49;
 	private ParseTree parseTree;
+	private SymbolTable symbolTable;
 
 	public Parser(Scanner scanner, Grammar grammar){
 		this.scanner = scanner;
 		this.grammar = grammar;
 		parseTree = new ParseTree();
 		buildParserTable();
+		symbolTable = new SymbolTable();
 	}
 
 	public boolean parseText() {
+		int currScope = 0;
+		String currFunc = "LET";
 		boolean debug = false;
+		ArrayList<String> typelessIDs = new ArrayList();
 		if (debug) 		System.out.println("-------------------------------STARTING PARSE-------------------------------");
 
 		Stack<ParserSymbol> symbolStack = new Stack<>();
@@ -70,7 +75,16 @@ public class Parser{
 					System.out.println("ERROR: Trying to match '" + parserSymbol + "', but found: '" + token + "' on line: " + token.lineNumber);
 					return false;
 				}
-
+				
+				if (productionRule.left().getNonTerminal() == NonTerminals.TYPE_DECLARATION){
+					buildTypeAndAddToTable();
+					continue;
+				}
+				
+				if(productionRule.left().getNonTerminal() == NonTerminals.VAR_DECLARATION){
+					buildVarAndAddToTable();
+					continue;
+				}
 
 				ParserSymbol rightSymbol = productionRule.right()[0];
 				if (productionRule.right().length == 1 &&
@@ -102,10 +116,87 @@ public class Parser{
 		}
 
 		System.out.println("\nSuccessful parse!!!");
-
+		// print symbol table on success
+		symbolTable.printSymbolTable();
 		return true;
 	}
-
+	
+	/*
+	 * Assumes last symbol popped was TYPE_DECLARATION
+	 */
+	private void buildTypeAndAddToTable(){
+		String typeName = "";
+		PrimitiveTypes.PrimitiveType primitiveType = null;
+		int arrSize = 0;
+		Token currToken;
+		currToken = scanner.nextToken();
+		while(currToken.type() != State.SEMI){ // semi marks end of type declaration
+			if(currToken.type() == State.ID){
+				if(currToken.value().equals("string")) primitiveType = PrimitiveTypes.PrimitiveType.STRING;
+				else if(currToken.value().equals("int")) primitiveType = PrimitiveTypes.PrimitiveType.INT;
+				else typeName = currToken.value();
+			}
+			else if(currToken.type() == State.INTLIT) arrSize = Integer.parseInt(currToken.value());
+			currToken = scanner.nextToken();
+		}
+		
+		symbolTable.addType(typeName, primitiveType, arrSize);
+	}
+	
+	/*
+	 *  Assumes last symbol popped was VAR_DECLARATION
+	 */
+	private void buildVarAndAddToTable(){
+		LinkedList<String> varNames = new LinkedList();
+		String typeName = null;
+		String varVal;
+		Token currToken;
+		currToken = scanner.nextToken();
+		while(currToken.type() != State.SEMI){
+			if(currToken.type() == State.ID){
+				varNames.add(currToken.value());
+			}
+			if(currToken.type() == State.COLON){
+				currToken = scanner.nextToken();
+				// should be type
+				if(currToken.type() == State.ID){
+					typeName = currToken.value();
+				}
+			}
+			if(currToken.type() == State.STRLIT || currToken.type() == State.INTLIT){
+				varVal = currToken.value();
+			}
+			currToken = scanner.nextToken();
+		}
+		
+		for(String varName : varNames){
+			symbolTable.addVar(varName, typeName);
+		}
+	}
+	
+	/*
+	 *  Assumes last symbol popped was FUNCT_DECLARATION
+	 */
+	private void buildFuncAndAddToTable(){
+		
+		Token currToken;
+		String funcName;
+		currToken = scanner.nextToken(); // func
+		boolean gotFuncName = false;
+		while(currToken.type() != State.BEGIN){
+			if(currToken.type() == State.ID){
+				if(!gotFuncName){
+					funcName = currToken.value();
+					symbolTable.beginScope(funcName);
+					gotFuncName = true;
+				}
+				else{
+					
+				}
+			}
+		}
+	}
+	
 	private void buildParserTable(){
 		parserTable = new ProductionRule[NUM_NONTERMINALS][NUM_TERMINALS];
 

FuncSymbolEntry.java
@@ -1,0 +1,17 @@
+import java.util.LinkedList;
+
+
+public class FuncSymbolEntry extends SymbolTableEntry{
+	
+	private LinkedList<VarSymbolEntry> params;
+	
+	public FuncSymbolEntry(String name, Scope scope, LinkedList<VarSymbolEntry> params) {
+		super(name, scope);
+		this.params = params;
+	}
+	
+	public LinkedList<VarSymbolEntry> getParams(){
+		return params;
+	}
+
+}

PrimitiveTypes.java
@@ -1,0 +1,7 @@
+
+public class PrimitiveTypes {
+	public enum PrimitiveType{
+		STRING,
+		INT
+	}
+}

Scope.java
@@ -1,0 +1,18 @@
+
+public class Scope {
+	private String funcName;
+	private int level;
+	
+	public Scope(String funcName, int level){
+		this.funcName = funcName;
+		this.level = level;
+	}
+	
+	public String getFuncName(){
+		return funcName;
+	}
+	
+	public int getLevel(){
+		return level;
+	}
+}

SymbolTable.java
@@ -1,13 +1,68 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class SymbolTable {
-	private ArrayList<SymbolTableEntry> symbolTable;
-
+	private Scope currScope = new Scope("LET", 0);
+	private HashMap<String, ArrayList<VarSymbolEntry>> vars;
+	private HashMap<String, ArrayList<FuncSymbolEntry>> functions;
+	private HashMap<String, TypeSymbolEntry> types;
+	
 	public SymbolTable() {
-		symbolTable = new ArrayList<SymbolTableEntry>();
+		vars = new HashMap<String, ArrayList<VarSymbolEntry>>();
+		functions = new HashMap<String, ArrayList<FuncSymbolEntry>>();
+		types = new HashMap<String, TypeSymbolEntry>();
+		types.put("int", new TypeSymbolEntry("int", currScope, PrimitiveTypes.PrimitiveType.INT, 0));
+		types.put("string", new TypeSymbolEntry("string", currScope, PrimitiveTypes.PrimitiveType.STRING, 0));
+	}
+	
+	public boolean containsVar(String varName){
+		return false;
 	}
 
-	public void add(String name, String type, String scope) {
-		symbolTable.add(new SymbolTableEntry(name, type, scope));
+	public void addVar(String name, String type){
+		if(!vars.containsKey(name)){
+			vars.put(name, new ArrayList<VarSymbolEntry>());
+		}
+		
+		vars.get(name).add(new VarSymbolEntry(name, currScope, types.get(type)));
 	}
+	
+	public void addType(String name, PrimitiveTypes.PrimitiveType primType, int arrSize ){
+			types.put(name, new TypeSymbolEntry(name, currScope, primType, arrSize));
+	}
+	
+	public void beginScope(String funcName){
+		
+	}
+	
+	public void endScope(){
+		
+	}
+	
+	public void printSymbolTable(){
+		System.out.println(":::::::PRINTING SYMBOL TABLE::::::::::");
+		System.out.println("== VARS ==");
+		for(String key : vars.keySet()){
+			ArrayList<VarSymbolEntry> scopeVars = vars.get(key);
+			for(VarSymbolEntry var : scopeVars){
+				System.out.println("VAR: " + var.toString());
+			}
+		}
+		System.out.println("== END VARS ==");
+		/*System.out.println("== FUNCS == ");
+		for(String key : functions.keySet()){
+			FuncSymbolEntry func = functions.get(key);
+			System.out.println("FUNC: " + func.toString());
+		}
+		System.out.println("== END FUNCS ==");*/
+		System.out.println("== TYPES == ");
+		for(String key : types.keySet()){
+			TypeSymbolEntry type = types.get(key);
+			System.out.println("TYPE: " + type.toString());
+		}
+		System.out.println("== END TYPES ==");
+		System.out.println("::::    END PRINT SYMBOL TABLE    :::::");
+	}
+	
 }

SymbolTableEntry.java
@@ -1,11 +1,9 @@
-public class SymbolTableEntry {
+public abstract class SymbolTableEntry {
 	private String name;
-	private String type;
-	private String scope;
+	private Scope scope;
 
-	public SymbolTableEntry(String name, String type, String scope) {
+	public SymbolTableEntry(String name, Scope scope) {
 		this.name = name;
-		this.type = type;
 		this.scope = scope;
 	}
 
@@ -13,11 +11,7 @@ public class SymbolTableEntry {
 		return name;
 	}
 	
-	public String getType() {
-		return type;
-	}
-
-	public String getScope() {
+	public Scope getScope() {
 		return scope;
 	}
 }

TypeSymbolEntry.java
@@ -1,0 +1,26 @@
+
+public class TypeSymbolEntry extends SymbolTableEntry{
+	
+	private PrimitiveTypes.PrimitiveType primitiveType;
+	private int arrSize;
+	
+	public TypeSymbolEntry(String name, Scope scope, PrimitiveTypes.PrimitiveType primitiveType, int arrSize){
+		super(name, scope);
+		this.primitiveType = primitiveType;
+		this.arrSize = arrSize;
+	}
+	
+	public PrimitiveTypes.PrimitiveType getPrimType(){
+		return primitiveType;
+	}
+	
+	public int getArrSize(){
+		return arrSize;
+	}
+	
+	public String toString(){
+		return "(name: " + this.getName() + " | level: " + this.getScope().getLevel() + " | func: "
+				+ this.getScope().getFuncName() + " | primType " + primitiveType.values()[primitiveType.ordinal()]+
+				" | arrSize " + arrSize +  ")";
+	}
+}

VarSymbolEntry.java
@@ -1,0 +1,16 @@
+
+public class VarSymbolEntry extends SymbolTableEntry {
+
+	private TypeSymbolEntry type;
+	
+	public VarSymbolEntry(String name, Scope scope, TypeSymbolEntry type) {
+		super(name, scope);
+		this.type = type;
+	}
+	
+	public String toString(){
+		return "(name: " + this.getName() + " | level: " + this.getScope().getLevel() + " | func: "
+				+ this.getScope().getFuncName() + " | type " + type.getName() + ")";
+	}
+	
+}

commit c4debc8b452039b21b5601f86a7517adb418c3a9

fixed parser control flow issue introduced in c78bef5a

Parser.java
@@ -89,15 +89,6 @@ public class Parser{
 					System.out.println("ERROR: Trying to match '" + parserSymbol + "', but found: '" + token + "' on line: " + token.lineNumber);
 					return false;
 				}
-				if (productionRule.left().getNonTerminal() == NonTerminals.TYPE_DECLARATION){
-					buildTypeAndAddToTable();
-					continue;
-				}
-
-				if(productionRule.left().getNonTerminal() == NonTerminals.VAR_DECLARATION){
-					buildVarAndAddToTable();
-					continue;
-				}
 
 				ParserSymbol rightSymbol = productionRule.right()[0];
 				if (productionRule.right().length == 1 &&
@@ -114,6 +105,12 @@ public class Parser{
 						if (debug) System.out.println(">> Parser Push: " + productionRule.right()[i]);
 						symbolStack.push(productionRule.right()[i]);
 					}
+				}
+
+				if (productionRule.left().getNonTerminal() == NonTerminals.TYPE_DECLARATION){
+					buildTypeAndAddToTable();
+				} else if (productionRule.left().getNonTerminal() == NonTerminals.VAR_DECLARATION){
+					buildVarAndAddToTable();
 				}
 			}
 		}
@@ -138,7 +135,7 @@ public class Parser{
 		PrimitiveTypes.PrimitiveType primitiveType = null;
 		int arrSize = 0;
 		Token currToken;
-		currToken = scanner.nextToken();
+		currToken = scanner.peekToken();
 		while(currToken.type() != State.SEMI){ // semi marks end of type declaration
 			if(currToken.type() == State.ID){
 				if(currToken.value().equals("string")) primitiveType = PrimitiveTypes.PrimitiveType.STRING;
@@ -146,7 +143,7 @@ public class Parser{
 				else typeName = currToken.value();
 			}
 			else if(currToken.type() == State.INTLIT) arrSize = Integer.parseInt(currToken.value());
-			currToken = scanner.nextToken();
+			currToken = scanner.peekToken();
 		}
 
 		symbolTable.addType(typeName, primitiveType, arrSize);
@@ -160,13 +157,13 @@ public class Parser{
 		String typeName = null;
 		String varVal;
 		Token currToken;
-		currToken = scanner.nextToken();
+		currToken = scanner.peekToken();
 		while(currToken.type() != State.SEMI){
 			if(currToken.type() == State.ID){
 				varNames.add(currToken.value());
 			}
 			if(currToken.type() == State.COLON){
-				currToken = scanner.nextToken();
+				currToken = scanner.peekToken();
 				// should be type
 				if(currToken.type() == State.ID){
 					typeName = currToken.value();
@@ -175,7 +172,7 @@ public class Parser{
 			if(currToken.type() == State.STRLIT || currToken.type() == State.INTLIT){
 				varVal = currToken.value();
 			}
-			currToken = scanner.nextToken();
+			currToken = scanner.peekToken();
 		}
 
 		for(String varName : varNames){
@@ -190,7 +187,7 @@ public class Parser{
 
 		Token currToken;
 		String funcName;
-		currToken = scanner.nextToken(); // func
+		currToken = scanner.peekToken(); // func
 		boolean gotFuncName = false;
 		while(currToken.type() != State.BEGIN){
 			if(currToken.type() == State.ID){