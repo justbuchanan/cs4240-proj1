5d2ab4e18c247cf9a2a3479f3afa4ea63c12505e
VirtualBoxImages.com <adminuser@adminuser.(none)>
2014-03-24 04:38:47 +0200

    Readded AST

--- Parser.java
+++ Parser.java
@@ -365,5 +365,6 @@ public class Parser{
 	public void printTree() {
 		System.out.println("Number of nodes in parse tree: " + parseTree.getSize());
 		System.out.println(parseTree.toString());
+		System.out.println(parseTree.getAST().toString());
 	}
 }
