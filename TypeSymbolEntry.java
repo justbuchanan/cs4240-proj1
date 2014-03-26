cd848c1434435538c913553237b3b5d0714e6542
Justin Buchanan <justbuchanan@gmail.com>
2014-03-26 02:27:19 -0400

    added typeString() to TypeSymbolEntry.  closes #17

--- TypeSymbolEntry.java
+++ TypeSymbolEntry.java
@@ -5,18 +5,36 @@ public class TypeSymbolEntry extends SymbolTableEntry{
 	private String eltType;
 	private int arrSize;
 	private ArrayList<Integer> arrDims;
+	private String typeString;
 	
 	public TypeSymbolEntry(String name, Scope scope, String eltType, ArrayList<Integer> arrDims) {
 		super(name, scope);
 
 		if (eltType == null) eltType = name;
 		this.eltType = eltType;
-		
+
 		this.arrDims = arrDims;
+
+		//	build typeString
+		this.typeString = this.eltType;
+		if (arrDims != null) {
+			for (Integer dim : arrDims) {
+				this.typeString += "[]";
+			}
+		}
 	}
 	
 	public String getEltType(){
 		return eltType;
+	}
+
+	/**
+	 * Describes the type.  Use it for comparison between other types.
+	 *
+	 * Examples: "int", "int[]", "int[][]", "string", "string[][][]"
+	 */
+	public String typeString() {
+		return typeString;
 	}
 	
 	public ArrayList<Integer> getArrDims(){
