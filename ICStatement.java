a7c5dd67b9f2720b2d56506637002dd23e63214f
Justin Buchanan <justbuchanan@gmail.com>
2014-03-29 20:55:41 -0400

    added ICStatement

---
+++ ICStatement.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+/**
+ * Represents a single line in the intermediate code.
+ * Can be an operation: op, y, z, x
+ * OR a label
+ */
+public class ICStatement {
+	private ArrayList<String> components;
+	private String labelName;
+
+	public boolean isLabel() {
+		return labelName != null;
+	}
+
+	public ICStatement(String labelName) {
+		this.labelName = labelName;
+	}
+
+	public ICStatement(String op, String outReg, String operand1Reg, String operand2Reg) {
+		components = new ArrayList<>();
+		components.add(op);
+		components.add(outReg);
+		components.add(operand1Reg);
+		components.add(operand2Reg);
+	}
+
+
+	//	Operation Statement
+	////////////////////////////////////////////////////////////////////////////////
+
+	public String getOperator() {
+		return components.get(0);
+	}
+
+	public String getOutputRegister() {
+		return components.get(1);
+	}
+
+	public String getLeftOperand() {
+		return components.get(2);
+	}
+
+	public String getRightOperand() {
+		return components.get(3);
+	}
+
+
+	//	Label Statement
+	////////////////////////////////////////////////////////////////////////////////
+
+	public String getLabelName() {
+		return labelName;
+	}
+}
