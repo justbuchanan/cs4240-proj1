af0f78d7d6f2839ff517c2f9152d025f94447e4b
Justin Buchanan <justbuchanan@gmail.com>
2014-04-22 01:52:48 -0400

    mostly implemented coloring algorithm

--- EbbRegisterAllocator.java
+++ EbbRegisterAllocator.java
@@ -1,5 +1,10 @@
 import java.util.Arrays;
 import java.util.Set;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Stack;
 import java.util.Iterator;
 import java.util.ArrayList;
 
@@ -17,18 +22,224 @@ public class EbbRegisterAllocator implements RegisterAllocator {
 	public ArrayList<CodeStatement> allocRegisters(ArrayList<CodeStatement> irCode) {
 		ControlFlowGraph cfg = new ControlFlowGraph(irCode);
 
-		for (ControlFlowGraph.ExtendedBasicBlock ebb : cfg.getExtendedBasicBlocks()) {
-			//	FIXME: def-use chains
 
-			for (ControlFlowGraph.BasicBlock bb : ebb.getBasicBlocks()) {
-				//	TODO
-			}
-		}
+		// ArrayList<VarUsageWeb> webs = new ArrayList<>();
+
+
+		// //	loop over all BasicBlocks
+		// //	for each def-ed variable, we add the start of a web to @webs
+		// for (ControlFlowGraph.ExtendedBasicBlock ebb : cfg.getExtendedBasicBlocks()) {
+		// 	for (ControlFlowGraph.BasicBlock bb : ebb.getBasicBlocks()) {
+
+		// 		for (int i = 0; i < bb.getCode().size(); i++) {
+		// 			CodeStatement stmt = bb.getCode().get(i);
+		// 			Set<String> defs = new HashSet<>();
+		// 			Set<String> uses = new HashSet<>();
+		// 			getVariableDefsAndUses(stmt, defs, uses);
+
+
+		// 			for (String deffedVar : defs) {
+		// 				VarUsageWeb webStart = new VarUsageWeb(deffedVar, bb, i);
+		// 				webs.add(webStart);
+		// 			}
+
+
+
+		// 			//	note: only the first basic block in the EBB can have more than one predecessor
+
+
+		// 			//	FIXME: use the defs and uses
+		// 		}
+
+		// 	}
+		// }
 
 		//	TODO
 
 		return finalCode;
 	}
+
+
+
+	public class InterferenceGraph<T> {
+		private ArrayList<T> nodes;
+
+		//	an edge is a Set containing two nodes
+		//	this allows the undirected-ness to be ignored with .equals()
+		private Set<Edge> edges;
+
+
+		private class Edge {
+			public int a, b;
+
+			public Edge(int a , int b) {
+				this.a = a;
+				this.b = b;
+			}
+
+			public boolean equals(Object obj) {
+				if (obj == null || !(obj.getClass() != this.getClass())) {
+					return false;
+				}
+
+				Edge other = (Edge) obj;
+				return (other.a == a && other.b == a) || (other.b == a && other.a == b);
+			}
+
+			public int hashCode() {
+				return a*a + b*b;
+			}
+
+			public boolean isConnectedTo(int nodeIdx) {
+				return a == nodeIdx || b == nodeIdx;
+			}
+		}
+
+
+		InterferenceGraph() {
+			this.nodes = new ArrayList<>();
+			this.edges = new HashSet<>();
+		}
+
+		public Map<T, String> color(Set<String> colors) {
+			Stack<T> stack = new Stack<>();
+			Set<T> unhandledNodes = new HashSet<>(nodes);
+			Map<T, String> assignments = new HashMap<>();
+
+			//	go through all nodes.  if a given node has < colors.size() edges,
+			//	remove it from @unhandledNodes and push it onto the stack
+			Iterator<T> itr = unhandledNodes.iterator();
+			while (itr.hasNext()) {
+				T n = itr.next();
+				int i = nodes.indexOf(n);
+				int count = countEdges(i, stack);
+
+				if (count < colors.size()) {
+					stack.push(n);
+					itr.remove();
+				}
+			}
+
+			if (unhandledNodes.size() > 0) {
+				//	TODO: implement splitting or spilling here
+				throw new RuntimeException("The allocator doesn't yet handle splitting...");
+			}
+
+			//	remove nodes from the stack, assigning colors as we go
+			while (stack.size() > 0) {
+				T n = stack.pop();
+
+				Set<T> coloredNeighbors = getNeighbors(nodes.indexOf(n));
+				coloredNeighbors.removeAll(stack);
+
+				Set<String> availableColors = new HashSet<>(colors);
+				for (T coloredNeighbor : coloredNeighbors) {
+					availableColors.remove(assignments.get(coloredNeighbor));
+				}
+
+				if (availableColors.size() == 0) {
+					throw new RuntimeException("Oops... there's no available colors");
+				}
+
+				//	choose a random color to assign
+				String colorToAssign = availableColors.iterator().next();
+				assignments.put(n, colorToAssign);
+			}
+
+			return assignments;
+		}
+
+		public Set<T> getNeighbors(int nodeNum) {
+			Set<T> neighbors = new HashSet<>();
+
+			for (Edge e : edges) {
+				if (e.a == nodeNum) {
+					neighbors.add(nodes.get(e.b));
+				} else if (e.b == nodeNum) {
+					neighbors.add(nodes.get(e.a));
+				}
+			}
+
+			return neighbors;
+		}
+
+		public int countEdges(int nodeNum, Collection<T> exclude) {
+			int count = 0;
+
+			for (Edge e : edges) {
+				if ( (e.a == nodeNum && !exclude.contains(nodes.get(e.b)))
+					|| (e.b == nodeNum && !exclude.contains(nodes.get(e.a))) ) {
+					count++;
+				}
+			}
+
+			return count;
+		}
+
+		public void addNode(T node) {
+			nodes.add(node);
+		}
+
+		public void addEdge(T from, T to) {
+			edges.add(new Edge(nodes.indexOf(from), nodes.indexOf(to)));
+		}
+
+		public String toGraphviz() {
+			String gv = "graph interferenceGraph {\n";
+
+			for (int i = 0; i < nodes.size(); i++) {
+				gv += "\tnode" + i + "[label=\"" + nodes.get(i).toString() + "\"];\n";
+			}
+
+			gv += "\n";
+
+			for (Edge e : edges) {
+				gv += "\tnode" + e.a + " -> node" + e.b + ";\n";
+			}
+
+			gv += "}";
+
+			return gv;
+		}
+	}
+
+
+	// private class VarUsageWeb {
+
+	// 	/**
+	// 	 * Create a web beginning with a def at the given location
+	// 	 */
+	// 	public VarUsageWeb(String varName, BasicBlock bb, int lineNumber) {
+
+	// 	}
+
+	// 	public void addUse(BasicBlock bb, int lineNumber) {
+
+	// 	}
+
+
+	// 	// public VarUsageNode addNode(String varName, int liveRangeIndex) {
+
+
+	// 	// }
+
+	// 	// public VarUsageEdge connect(VarUsageNode node1, VarUsageNode node2);
+
+
+	// 	// public class VarUsageEdge {
+
+	// 	// }
+
+	// 	// public class VarUsageNode {
+	// 	// 	public int basicBlockIndex;
+	// 	// 	public int lineIndex;		//	index into the basic blocks
+	// 	// }
+	// }
+
+
+
+
+
 
 	/**
 	 * Examines the given code statement and finds the variables defined and used, returning them through the output variables.
@@ -102,21 +313,6 @@ public class EbbRegisterAllocator implements RegisterAllocator {
 			}
 		}
 	}
-
-	// private class VarAccess {
-	// 	public String varName;
-	// 	public AccessType accessType;
-
-	// 	public enum AccessType {
-	// 		AccessTypeUse,
-	// 		AccessTypeDef
-	// 	};
-
-	// 	public VarAccess(AccessType accessType, String varName) {
-	// 		this.accessType = accessType;
-	// 		this.varName = varName;
-	// 	}
-	// }
 
 
 	public void printCode() {
